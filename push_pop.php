e9b0fb81a0161e4bd50d6dc7742af88fd45bef07
FerreWagner <18408229270@163.com>
2018-08-13 11:40:05 +0800

    add new test

--- push_pop.php
+++ push_pop.php
@@ -24,4 +24,24 @@ foreach ($arr as $k => $v){
 }
 
 array_shift($array_other);
-print_r($array_other);
+//print_r($array_other);
+
+//冒泡
+function BubbleSort($arr){
+    $length = count($arr);
+    if($length<=1) return $arr;
+
+    for($i=0;$i<$length;$i++){
+        for($j=$length-1;$j>$i;$j--){
+//            echo $arr[$j];die;
+            if($arr[$j]<$arr[$j-1]){
+                $tmp = $arr[$j];
+                $arr[$j] = $arr[$j-1];
+                $arr[$j-1] = $tmp;
+            }
+        }
+    }
+    return $arr;
+}
+
+print_r(BubbleSort([5,8,3,9,7]));
